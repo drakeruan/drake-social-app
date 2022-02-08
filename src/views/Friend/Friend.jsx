34e75831c2aaa12a24d3cd6ccdd282e371c763f7
longnhp <longnhp62@wru.vn>
2022-02-08 17:59:20 +0700

    Add actions friend item :+1:

--- src/views/Friend/Friend.jsx
+++ src/views/Friend/Friend.jsx
@@ -11,9 +11,18 @@ const Friend = () => {
         <p className="friend-item-brief-info-name">Nguyễn Văn A</p>
         <p className="friend-item-brief-info-mutual">10 bạn chung</p>
       </div>
-      <button className="friend-item-brief-info-btn">
-        <i className='bx bx-dots-horizontal-rounded'></i>
-      </button>
+      <div className="friend-item-brief-actions">
+        <button className="friend-item-brief-actions-btn">
+          <i className='bx bx-dots-horizontal-rounded'></i>
+        </button>
+        <div className="friend-item-brief-actions-list">
+          <ul>
+            <li className="friend-item-brief-actions-list-item">Xem trang cá nhân</li>
+            <li className="friend-item-brief-actions-list-item">Xóa bạn bè</li>
+            <li className="friend-item-brief-actions-list-item">Nhắn tin</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>;
 };
